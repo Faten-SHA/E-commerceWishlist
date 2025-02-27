cf0312bb87efce9d970aca6e03373c0008ed8281
Faten-SHA <fatenlahlouh@gmail.com>
2025-02-27 14:41:40 +0300

    Setup Express Server

--- WishCartApp/backend/server.js
+++ WishCartApp/backend/server.js
@@ -1,1 +1,17 @@
- 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const WishItem = require("./models/wishItemModel");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//middleware --- json parser
+app.use(express.json());
+app.use(cors());
+
+//route prefix
+//app.use("", require("./routes/routes.js"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

a5688ff109ddc16be1de4cf53c5dd1cd1be11db9
Faten-SHA <fatenlahlouh@gmail.com>
2025-02-27 14:31:25 +0300

    Prepare WishItem schema in wishItem model

---
+++ WishCartApp/backend/models/wishItemModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const wishItemSchema = new mongoose.Schema(
+    {
+        wishItemName: { type: String, required: true },
+        wishItemPrice: { type: Number, required: false },
+        wishItemDescription: { type: String, required: false },
+        wishItemURL: { type: String, required: true },
+        wishItemCategory: { type: String, required: true },
+        wishItemPriority: { type: String, required: true },
+        isPurchased: { type: Boolean, required: true },
+
+        
+        
+    },
+    {
+        timestamps: true,
+    }
+)
+
+// Create a model named 'WishItem' using the defined schema
+const WishItem = mongoose.model('WishItem', wishItemSchema);
+
+// Export the model
+module.exports = WishItem;
